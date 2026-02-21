2e84de5af0f56549bc616a02d773a23e9bc3c216
muaaziqbalvip <muaaziqbal.vip@gmail.com>
2026-02-21 16:25:10 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'mi-ramadan-v1';
 const ASSETS = [
-  'ramadan.html',
+  'index.html',
   'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;700;900&family=Poppins:wght@100;300;400;600;800&family=Amiri:wght@400;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js'
 ];
